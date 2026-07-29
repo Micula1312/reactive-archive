5d5c4a26801dd7b7b110e614b0753b76260021a6
Micula31 <tecnoboskiva1312@proton.me>
2026-07-29 19:36:12 +0200

    configure Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://micula1312.github.io",
+  base: "/reactive-archive",
   vite: {
     define: {
       global: {}
