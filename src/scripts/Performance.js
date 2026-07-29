e0cdd7945fbce7c90c7e606c0b2d0a1b3da07a96
Micula31 <tecnoboskiva1312@proton.me>
2026-07-29 15:46:40 +0200

    feat: select performance from url

--- src/scripts/Performance.js
+++ src/scripts/Performance.js
@@ -1,4 +1,17 @@
 import performance01 from "../performances/performance-01/index.js";
+import elisa from "../performances/elisa/index.js";
+import { normalizePerformance } from "./Cue.js";
 
-export const performance = performance01;
-export default performance01;
+export const performances = {
+  "performance-01": normalizePerformance(performance01),
+  elisa: normalizePerformance(elisa)
+};
+
+const requestedPerformance = new URLSearchParams(window.location.search).get("performance");
+const selectedId = requestedPerformance && performances[requestedPerformance]
+  ? requestedPerformance
+  : "performance-01";
+
+export const performanceId = selectedId;
+export const performance = performances[selectedId];
+export default performance;
