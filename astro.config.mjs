90839d36e212d9acd6674eba91d1a509df2e47e3
micolg <micolg@gmail.com>
2026-07-29 20:12:04 +0200

    fix astro config conflict

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://thearchiveoftheuntamed.xyz",
-<<<<<<< Updated upstream
-=======
   base: "/reactive-archive",
->>>>>>> Stashed changes
 
   vite: {
     define: {
