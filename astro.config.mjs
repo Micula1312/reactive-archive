58f8239a046c33dcddb8f75c766b89935495e0fd
micolg <micolg@gmail.com>
2026-07-29 20:09:51 +0200

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://thearchiveoftheuntamed.xyz",
+<<<<<<< Updated upstream
+=======
+  base: "/reactive-archive",
+>>>>>>> Stashed changes
 
   vite: {
     define: {
